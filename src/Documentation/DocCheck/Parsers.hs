cc3fd3435a94eb20cad15570d147f9cfc9c49cdb
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-08-14 04:54:44 +0100

    Make some helpers for far more robust parsing

--- src/Documentation/DocCheck/Parsers.hs
+++ src/Documentation/DocCheck/Parsers.hs
@@ -9,12 +9,14 @@
 
 {-# LANGUAGE OverloadedStrings, ExistentialQuantification #-}
 module Documentation.DocCheck.Parsers
-    (escapingEmph, htmlEmph, WarningMessage, WarningParser(..))
+    (escapingEmph, htmlEmph, WarningMessage, WarningParser(..), skipUntil)
     where
 
-import           Control.Applicative ((*>))
+import           Control.Applicative ((*>), some, empty, many, (<|>), (<*))
 import qualified Data.Attoparsec.Text as A
-import           Data.Text (Text)
+import           Data.Text           (Text, head, cons)
+
+type Parser = A.Parser
 
 -- | Warning message used when parsing succeeds.
 type WarningMessage = String
@@ -23,10 +25,10 @@ type WarningMessage = String
 -- success.
 data WarningParser = forall a. WarningParser (A.Parser a) WarningMessage
 
--- | Escape character \\\\ inside of emphasis.
+-- | Looks for the escape character inside of emphasis pairs
 escapingEmph :: WarningParser
-escapingEmph = WarningParser escapingEmph'
-               "attempting to escape a character inside of emphasis"
+escapingEmph = WarningParser emphBlock'
+             "escape inside of emphasis"
 
 -- | HTML escape sequence inside of emphasis.
 htmlEmph :: WarningParser
@@ -36,6 +38,45 @@ htmlEmph = WarningParser htmlEmph'
 escapingEmph' :: A.Parser Text
 escapingEmph' = A.takeWhile (/= '/') *> "/"
                 *> A.takeWhile (`notElem` "\n\\/") *> "\\"
+
+emphBlock' :: A.Parser Char
+emphBlock' = A.takeWhile (/= '/') *> block *> A.anyChar
+  where
+    block = A.scan False p
+      where
+        p inside c
+          | c == '/' = Just (not inside)
+          | c == '\n' = Just False
+          | c == '\\' && inside = Nothing
+          | otherwise = Just inside
+
+-- | Returns sections of text delimited by specified text.
+-- getBlocks :: Text -> Text -> Parser [Text]
+getBlocks
+  :: Text -- ^ Opening delimiter
+     -> Text -- ^ Closing delimiter
+     -> A.Parser [Text]
+getBlocks op ed = some $ skipUntil (Data.Text.head op) *> (start *> mid)
+  where
+    start = A.string op <|> A.anyChar *> start
+    mid :: A.Parser Text
+    mid = A.string ed *> return "" <|> do
+      c <- A.anyChar
+      m <- mid
+      return $ c `cons` m
+
+-- | Skip all characters until the specified one. Skips over
+-- the character if it's escaped.
+skipUntil :: Char -> A.Parser ()
+skipUntil c = A.char '\\' *> A.anyChar *> skipUntil c -- honour escapes
+              <|> peekTerminate -- if we get what we want, terminate
+              <|> A.anyChar *> skipUntil c -- skip next char, recurse
+  where
+    peekTerminate = do
+      x <- A.peekChar
+      case x of
+        Nothing -> fail "eof"
+        Just x' -> if x' == c then return () else fail "not goal"
 
 htmlEmph' :: A.Parser Text
 htmlEmph' = A.takeWhile (/= '/') *> "/"
